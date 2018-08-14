78b63f26707d1b20da41e4b8c3b7249973c88fb2
Caglar KOPARIR <ckoparir@gmail.com>
2018-08-14 08:34:46 +0300

    Tutor3

--- Tutor2/main.c
+++ Tutor2/main.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  int id;
+  long len;
+  char* data;
+} DataFile;
+
+DataFile* writedatafile(char*);
+void printdatafile(DataFile*);
 
 int main()
 {
+  DataFile* dataFile;
+  char *data = "This is a test data...\n";
+  
+  dataFile = writedatafile(data);
+  printdatafile(dataFile);
+
+  free(dataFile);
   
   return 0;
+}
+
+DataFile* writedatafile(char* data){
+
+  DataFile* dataFile = malloc(sizeof(DataFile));
+  
+  dataFile->id = rand();
+  dataFile->len = sizeof(DataFile);
+  dataFile->data = data;
+
+  return dataFile;
+}
+
+void printdatafile(DataFile* df){
+  printf("ID: %d\n", df->id);
+  printf("Total Lenght: %ld bytes\n", df->len);
+  printf("Data: %s", df->data);
 }
